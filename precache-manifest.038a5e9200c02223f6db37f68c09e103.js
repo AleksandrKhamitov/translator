dcc90d7d0e2325ff3a84a1988ec8e575a2fc12ff
Aleksandr <khamitov.aleksandr@gmail.com>
2022-07-12 15:46:32 +0200

    Add files via upload

---
+++ precache-manifest.038a5e9200c02223f6db37f68c09e103.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05254c15d542bb54f9f702c1b41cf886",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a26eddfc69f065743ae9",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "4d2767be334604967cba",
+    "url": "/static/js/2.e7a989e8.chunk.js"
+  },
+  {
+    "revision": "a26eddfc69f065743ae9",
+    "url": "/static/js/main.24f53c82.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
